4272f5135eb3377958d7d6992a61caf4c56cec1f
wuyeelok <wuyeelok@gmail.com>
2019-05-26 18:11:03 +0800

    Add test cases to test mvn test

--- src/main/java/com/kenneth/App.java
+++ src/main/java/com/kenneth/App.java
@@ -2,6 +2,16 @@ package com.kenneth;
 
 public class App {
 	public String returnSomething() {
-		return "DUMMY";
+		return "DUMMY: " + parseToInt("00000005", 0) + "!";
+	}
+
+	public int parseToInt(String stringToParse, int defaultValue) {
+		int phasedInt;
+		try {
+			phasedInt = Integer.parseInt(stringToParse);
+		} catch (NumberFormatException ex) {
+			phasedInt = defaultValue; // Use default value if parsing failed
+		}
+		return phasedInt;
 	}
 }
--- src/test/java/com/kenneth/AppTest.java
+++ src/test/java/com/kenneth/AppTest.java
@@ -1,13 +1,58 @@
 package com.kenneth;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class AppTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented!!!!");
+	public void test1() {
+		App app = new App();
+
+		int actualResult = app.parseToInt("00000011", 0);
+		int expectedResult = 11;
+
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void test2() {
+		App app = new App();
+
+		int actualResult = app.parseToInt("00000199", 0);
+		int expectedResult = 199;
+
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void test3() {
+		App app = new App();
+
+		int actualResult = app.parseToInt("00000000", 0);
+		int expectedResult = 0;
+
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void test4() {
+		App app = new App();
+
+		int actualResult = app.parseToInt("abc", 0);
+		int expectedResult = 0;
+
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void test5() {
+		App app = new App();
+
+		int actualResult = app.parseToInt(null, 0);
+		int expectedResult = 0;
+
+		assertEquals(expectedResult, actualResult);
 	}
 }
